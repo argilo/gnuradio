a0c6e96d011161047a29a185e8001fe28db65466
Clayton Smith <argilo@gmail.com>
2022-05-15 14:30:24 -0400

    runtime: Replace Boost mutexes with standard C++
    Signed-off-by: Clayton Smith <argilo@gmail.com>

--- gnuradio-runtime/python/gnuradio/gr/bindings/basic_block_python.cc
+++ gnuradio-runtime/python/gnuradio/gr/bindings/basic_block_python.cc
@@ -14,7 +14,7 @@
 /* BINDTOOL_GEN_AUTOMATIC(0)                                                       */
 /* BINDTOOL_USE_PYGCCXML(0)                                                        */
 /* BINDTOOL_HEADER_FILE(basic_block.h)                                             */
-/* BINDTOOL_HEADER_FILE_HASH(97ef3f809e58b459c57d41bab36bb97d)                     */
+/* BINDTOOL_HEADER_FILE_HASH(8a1081fa4cd5aed6aff97d45ee4f959d)                     */
 /***********************************************************************************/
 
 #include <pybind11/complex.h>
